bec32f99e103d688d3c9ff84f53b165faaf710e7
Alvee <alvee.rucse@gmail.com>
2024-05-13 19:49:10 +0600

    recursion problem

--- C_C.cpp
+++ C_C.cpp
@@ -2,13 +2,24 @@
 using namespace std;
 #define ll long long
 
-int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        string s;
-        cin>>s;
-        
+ll f(ll n, ll x, vector<ll>& a, vector<ll>& p) {
+    if (n == 0) {
+        return (x <= 0) ? 0 : 1;
+    } else if (x <= 1 + a[n - 1]) {
+        return f(n - 1, x - 1, a, p);
+    } else {
+        return p[n - 1] + 1 + f(n - 1, x - 2 - a[n - 1], a, p);
     }
+}
+
+int main() {
+    ll n, x;
+    cin >> n >> x;
+    vector<ll> a = {1}, p = {1};
+    for (int i = 0; i < n; ++i) {
+        a.push_back(a[i] * 2 + 3);
+        p.push_back(p[i] * 2 + 1);
+    }
+    cout << f(n, x, a, p) << endl;
     return 0;
 }
